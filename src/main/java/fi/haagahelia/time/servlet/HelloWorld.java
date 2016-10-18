7454039813f885ec16450cc5075425f4d7cbfe07
Alexandru Oat <alexandru.oat@myy.haaga-helia.fi>
2016-10-18 14:25:57 +0300

    Show time and update it every 2 sec

---
+++ src/main/java/fi/haagahelia/time/servlet/HelloWorld.java
@@ -0,0 +1,45 @@
+package fi.haagahelia.time.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/hello")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorld() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.print("Hello World!");
+		out.print("<meta http-equiv=\"refresh\" content=\"1\" />");
+		out.print(new Date());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/main/java/fi/haagahelia/time/util/HelloWorld.java
@@ -0,0 +1,16 @@
+package fi.haagahelia.time.util;
+/**
+ * 
+ *
+ * @author Alexandru Oat
+ * @version 18.10.2016
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// print in console
+		System.out.println("Hello World!");
+	}
+
+}
